03774a1cd3731c27d0f0ca14157d82e8e7733860
Academy_intern <academy_intern@Academy-interns-MacBook-Air.local>
2020-06-19 16:51:00 +0200

    post component updated

--- MDB-Angular-Free/src/app/app.module.ts
+++ MDB-Angular-Free/src/app/app.module.ts
@@ -2,21 +2,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { FormsModule } from '@angular/forms';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { PostUpdateComponent } from './components/post-update/post-update.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostUpdateComponent,
+ 
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MDBBootstrapModule.forRoot(),
     FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
