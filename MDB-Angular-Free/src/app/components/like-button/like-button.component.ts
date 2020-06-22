be0516549104f3712429b668c4b33c862455eb59
crutchbandicoot <tsheposet@hotmail.com>
2020-06-22 12:56:19 +0200

    Updated the like component

--- MDB-Angular-Free/src/app/components/like-button/like-button.component.ts
+++ MDB-Angular-Free/src/app/components/like-button/like-button.component.ts
@@ -6,18 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./like-button.component.scss']
 })
 export class LikeButtonComponent implements OnInit {
-
+  // Variables
   counter: any;
   count: any = 0;
+  isActive: boolean;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // Method for liking and unliking a post
+  // Fix the button color activation
   clickCount() {
     if (this.count === 0) {
       this.count = this.count + 1;
       this.counter = this.count;
+      this.isActive = !this.isActive;
       console.log(this.counter);
     } else {
       this.count = this.count - 1;
---
+++ MDB-Angular-Free/src/app/mock-post.ts
@@ -0,0 +1,59 @@
+import { Post } from './social';
+
+export const POST: Post[] = [
+    {
+      firstName: 'Tshepo',
+      lastName: 'Setshedi',
+      post: {
+          post1: 'This is the first post',
+          post2: 'this is the second post'
+      }
+    },
+    {
+        firstName: 'Kabelo',
+        lastName: 'Thomas',
+        post: {
+            post1: 'This is the first post',
+            post2: 'this is the second post'
+        }
+    },
+    {
+        firstName: 'Mike',
+        lastName: 'Williams',
+        post: {
+            post1: 'This is the first post',
+            post2: 'this is the second post'
+        }
+    },
+    {
+        firstName: 'John',
+        lastName: 'Doe',
+        post: {
+            post1: 'This is the first post',
+            post2: 'this is the second post'
+        }
+    },
+    {
+        firstName: 'Tshidi',
+        lastName: 'Makafane',
+        post: {
+            post1: 'This is the first post',
+            post2: 'this is the second post'
+        }
+    },
+    {
+        firstName: 'Godirayaone',
+        lastName: 'Setshedi',
+        post: {
+            post1: 'This is the first post',
+            post2: 'this is the second post'
+        }
+    },
+    {
+        firstName: 'Pajane',
+        lastName: 'Setshedi',
+        post: {
+            post1: 'This is the first post',
+            post2: 'this is the second post'
+        }
+    }];
